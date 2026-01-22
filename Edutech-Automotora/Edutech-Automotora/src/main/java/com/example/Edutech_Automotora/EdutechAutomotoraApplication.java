92e55e15557a9d4418662552dd3cf9a0628b5a69
Javier <ja.barriose@duocuc.cl>
2026-01-22 02:23:38 -0300

    Add files via upload

---
+++ Edutech-Automotora/Edutech-Automotora/src/main/java/com/example/Edutech_Automotora/EdutechAutomotoraApplication.java
@@ -0,0 +1,13 @@
+package com.example.Edutech_Automotora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutechAutomotoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdutechAutomotoraApplication.class, args);
+	}
+
+}
